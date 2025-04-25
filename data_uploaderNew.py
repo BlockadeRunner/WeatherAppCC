c0cd89bd82966c6ae63a58708c2912ad7ccf32f2
Dillon Patrick Buyrn <143207984+DillonB281@users.noreply.github.com>
2025-04-25 18:42:21 -0400

    Add files via upload

---
+++ data_uploaderNew.py
@@ -0,0 +1,39 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate('./weatherapp-d290b-firebase-adminsdk-fbsvc-607ca2c76b.json')
+
+from datetime import datetime
+
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+weatherdata_ref = db.collection('WeatherData')
+
+#Example input string, change to actual input method
+input_string = "temperature: 17.15, pressure: 1016.50, wetness: 1023"
+
+
+data = { "temperature": 0.0, "pressure": 0.0, "wetness": 0.0 }
+
+split_list = []
+
+#Split the string on commas then colons
+for metric in input_string.split(", "):
+    split_list += metric.split(": ")
+
+#use the first string as a dictionary key, then set the following 
+#number as its value
+i = 0
+while i < len(split_list):
+    data[split_list[i]] = float(split_list[i + 1])
+    i = i + 2
+
+print(data)
+
+#Upload the data to the database
+weatherdata_ref = weatherdata_ref.add({
+    'Temperature': data["temperature"],
+    'Pressure': data["pressure"],
+    'Wetness Value': data["wetness"],
+    'Time': firestore.SERVER_TIMESTAMP
+})
